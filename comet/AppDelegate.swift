07dbe040f9a737c021620e993e51d35d0d436b69
hiroaki Iwai <hiro_lily_da@hotmail.com>
2020-05-17 23:44:39 +0900

    新規コミット

---
+++ comet/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  comet
+//
+//  Created by 岩井 宏晃 on 2020/05/17.
+//  Copyright © 2020 koalab. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ comet/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  comet
+//
+//  Created by 岩井 宏晃 on 2020/05/17.
+//  Copyright © 2020 koalab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
