09ecdf5c2bd9d4a2695619a1508441f2328c354e
hiroaki Iwai <hiro_lily_da@hotmail.com>
2020-06-07 23:17:32 +0900

    リポジトリのプルリクエストの内容を取得して更新するクラスを実装

---
+++ comet/Model/repository/Repository.swift
@@ -0,0 +1,101 @@
+//
+//  Repository.swift
+//  comet
+//
+//  Created by 岩井 宏晃 on 2020/06/07.
+//  Copyright © 2020 koalab. All rights reserved.
+//
+
+import Foundation
+
+protocol RepositoryNotification: class {
+    func didUpdate(repository: Repository)
+}
+
+class Repository {
+    private var isUpdating = false
+    private var lastUpdated = Date()
+    private var timer = Timer()
+    
+    private let repositoryOwner: String
+    private let repositorySlug: String
+    private let userName: String
+    private let password: String
+    private let updateInterval: TimeInterval = 60.0
+    
+    private var observerList = [RepositoryNotification]()
+    
+    init(repositoryOwner: String, repositorySlug: String, userName: String, password: String) {
+        self.repositoryOwner = repositoryOwner
+        self.repositorySlug = repositorySlug
+        self.userName = userName
+        self.password = password
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { _ in
+            if (!self.isUpdating && self.shouldUpdate()) {
+                self.updatePullRequest()
+            }
+        })
+    }
+    
+    func stopTimer() {
+        timer.invalidate()
+    }
+    
+    func updatePullRequest() {
+        if (isUpdating) { return }
+        
+        isUpdating = true
+        DispatchQueue.global().async {
+            do {
+                defer { DispatchQueue.main.async {self.isUpdating = false} }
+                
+                let pullRequestList = try self.fetchPullRequestList()
+                
+                DispatchQueue.main.async {
+                    self.lastUpdated = Date()
+                    print(pullRequestList)
+                }
+            } catch {
+                DispatchQueue.main.async { print("\(error.localizedDescription)") }
+            }
+        }
+    }
+    
+    func addObserver(observer: RepositoryNotification) {
+        if observerList.firstIndex(where: { $0 === observer }) == nil {
+            observerList.append(observer)
+        }
+    }
+    
+    func removeObserver(observer: RepositoryNotification) {
+        if let index = observerList.firstIndex(where: { $0 === observer }) {
+            observerList.remove(at: index)
+        }
+    }
+    
+    private func shouldUpdate() -> Bool {
+        return Date().timeIntervalSince1970 - lastUpdated.timeIntervalSince1970 >= updateInterval
+    }
+    
+    private func fetchPullRequestList() throws -> [ShowPullRequestResponse] {
+        let pullRequestIndex = try CallFetchPullRequests(
+            repositoryOwner: repositoryOwner,
+            repositorySlug: repositorySlug,
+            userName: userName,
+            password: password
+        ).execute()
+        
+        return try pullRequestIndex.values.map { (value) -> ShowPullRequestResponse in
+            try CallShowPullRequest(
+                id: value.id,
+                repositoryOwner: repositoryOwner,
+                repositorySlug: repositorySlug,
+                userName: userName,
+                password: password
+            ).execute()
+        }
+    }
+}
