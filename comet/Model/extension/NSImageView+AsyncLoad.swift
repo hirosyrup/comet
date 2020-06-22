41eb96e58af8be612e24affa8410e4cd3cf54d03
hiroaki Iwai <hiro_lily_da@hotmail.com>
2020-06-23 01:00:28 +0900

    画像のキャッシュ処理を実装

--- comet/Model/extension/NSImageView+AsyncLoad.swift
+++ comet/Model/extension/NSImageView+AsyncLoad.swift
@@ -9,13 +9,21 @@
 import Cocoa
 
 extension NSImageView {
+    static let imageCache = NSCache<AnyObject, AnyObject>()
+    
     func loadImageAsynchronously(url: URL) -> Void {
+        if let imageFromCache = NSImageView.imageCache.object(forKey: url as AnyObject) as? NSImage {
+            self.image = imageFromCache
+            return
+        }
+        
         image = nil
         DispatchQueue.global().async {
             let imageData: Data? = try? Data(contentsOf: url)
             DispatchQueue.main.async {
-                if let data = imageData {
-                    self.image = NSImage(data: data)
+                if let data = imageData, let loadedImage = NSImage(data: data) {
+                    self.image = loadedImage
+                    NSImageView.imageCache.setObject(loadedImage, forKey: url as AnyObject)
                 }
             }
         }
